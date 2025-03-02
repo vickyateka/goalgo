b45897a174b900899fba8a708d501921ee120183
vickyateka <vicky.kumar@eka.care>
2025-03-02 23:56:42 +0530

    lc 2563 binary search question

--- binary_search/lc2563_count_the_number_of_fair_pair.go
+++ binary_search/lc2563_count_the_number_of_fair_pair.go
@@ -1,64 +1,57 @@
+package main
 
+import (
+	"fmt"
+	"sort"
+)
 
-func countFairPairs(arr []int, lower int, upper int) int64 {
-    n := len(arr)
-    sort.Slice(arr, func(i, j int) bool {
-        return arr[i] < arr[j]
-    })
-
-    lower_bound := func(v []int, l, r, val int) int{
-        res := -1
-        for l <= r{
-            m := (l + r) / 2
-            if v[m] >= val {
-                res = m
-                r = m - 1
-            }else{
-                l = m + 1
-            }
-        }
-
-        return res
-    }
-
-    upper_bound := func(v []int, l, r, val int) int{
-        res := -1
-        for l <= r{
-            m := (l + r) / 2
-            if v[m] <= val {
-                res = m
-                l = m + 1
-            }else{
-                r = m - 1
-            }
-        }
-
-        return res
-    }
-
-    var ans int64
-    for i := 0; i < n; i ++{
-        y := upper_bound(arr, i + 1, n - 1, upper - arr[i])
-        x := lower_bound(arr, i + 1, n - 1, lower - arr[i])
-
-        if x >= 0 && y >= 0 {
-            ans += int64(y - x + 1)
-        }
-    }
-
-    return ans
+func lowerBound(v []int, l, r, val int) int {
+	for l <= r {
+		m := (l + r) / 2
+		if v[m] >= val {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	return l
 }
 
-// 0, 1, 2, 3, 4, 5
-// 0, 1, 4, 4, 5, 7
-// l = 3, u = 6
+func upperBound(v []int, l, r, val int) int {
+	for l <= r {
+		m := (l + r) / 2
+		if v[m] > val {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
 
-// b = lower_bound(u - a[i])
-// a = lower_bound(l + a[i])
-// ans += (b - a + 1)
+// Count fair pairs
+func countFairPairs(arr []int, lower int, upper int) int64 {
+	n := len(arr)
+	sort.Ints(arr) // Sort the array
 
-func main(){
+	var ans int64
+	for i := 0; i < n; i++ {
+		x := lowerBound(arr, i+1, n-1, lower-arr[i])
+		y := upperBound(arr, i+1, n-1, upper-arr[i]) - 1
 
-    // call the function with the actual input values. 
-    // take over from here
+		if x <= y {
+			ans += int64(y - x + 1)
+		}
+	}
+	return ans
+}
+
+func main() {
+	// Example test case
+	arr := []int{1, 2, 3, 4, 5}
+	lower := 2
+	upper := 5
+
+	result := countFairPairs(arr, lower, upper)
+	fmt.Println("Count of Fair Pairs:", result)
 }
